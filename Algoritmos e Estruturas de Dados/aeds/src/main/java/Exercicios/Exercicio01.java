2a648313e466892a8a44c3f243ed1f4726b42e00
Vitor Hugo Oliveira Sanson <vhsanson@gmail.com>
2024-12-13 20:01:08 -0300

    Update Exercicio01.java

--- Algoritmos e Estruturas de Dados/aeds/src/main/java/Exercicios/Exercicio01.java
+++ Algoritmos e Estruturas de Dados/aeds/src/main/java/Exercicios/Exercicio01.java
@@ -2,6 +2,13 @@ package Exercicios;
 
 import java.util.Scanner;
 
+/* Exercício 1: Ordenação por Inserção Escreva um programa que implemente o algoritmo de ordenação por inserção (Insertion Sort). 
+ * 1. Ler um vetor de números inteiros de tamanho n fornecido pelo usuário. 
+ * 2. Ordenar o vetor utilizando o algoritmo de Inserção.
+ * 3. Exibir o vetor antes e depois da ordenação.
+ * 4. Conte o número de comparações realizadas durante a ordenação e exiba esse valor ao final. 
+*/
+
 public class Exercicio01 {
     public static void main(String[] args) {
        Scanner entrada = new Scanner (System.in);
@@ -35,7 +42,7 @@ public class Exercicio01 {
             while (j >= 0 && vetor[j] > auxiliar) {
                 vetor [j+1] =vetor[j];
                 j--;
-                contador++;
+                contador++;  // Conta o número de comparações realizadas durante a ordenação e exiba esse valor ao final
             }
             contador++;
             vetor[j+1] = auxiliar;
