b437c9e31ae3801be90458d3975d640f14183956
Vitor Hugo Oliveira Sanson <vhsanson@gmail.com>
2024-12-13 18:51:54 -0300

    Add files via upload

---
+++ Algoritmos e Estruturas de Dados/aeds/src/main/java/Exercicios/Exercicio01.java
@@ -0,0 +1,44 @@
+package Exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio01 {
+    public static void main(String[] args) {
+       Scanner entrada = new Scanner (System.in);
+       
+       System.out.print("Digite o tamnho do vetor: ");
+       int tamanhoVetor = entrada.nextInt();
+       
+       System.out.print("\nDigite os valores do vetor: ");
+       int vetor[] = new int [tamanhoVetor];
+       
+        for (int i = 0; i < tamanhoVetor; i++) {
+            vetor[i] = entrada.nextInt();   
+        }
+        ordenaVetor(tamanhoVetor,vetor);
+        
+        System.out.print("Vetor: ");
+        for (int i = 0; i < tamanhoVetor; i++) {
+            System.out.print(vetor[i]);    
+            
+        }
+    } 
+    
+    //ordenar o vetor usando o método de inserção
+    public static void ordenaVetor(int tamanho, int vetor[]){
+        
+        for (int i = 1; i < tamanho; i++) {
+            int auxiliar = vetor[i];
+            int j = i-1;
+            
+            //Comparar e mover elementos
+            while (j >= 0 && vetor[j] > auxiliar) {
+                vetor [j+1] =vetor[j];
+                j--;
+            }
+            
+            vetor[j+1] = auxiliar;
+            
+        }
+    }
+}
---
+++ Algoritmos e Estruturas de Dados/aeds/src/main/java/com/mycompany/aeds/Aeds.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.aeds;
+
+/**
+ *
+ * @author 0052578
+ */
+public class Aeds {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
